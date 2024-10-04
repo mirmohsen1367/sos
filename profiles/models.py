c41d5519eca8816d6d64c977db67af00d685174a
mirmohsen <mirmohsenmosavi1367@gmail.com>
2024-10-04 12:13:56 +0330

    add profile app

--- profiles/models.py
+++ profiles/models.py
@@ -3,19 +3,22 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from profiles.custom_manager import CustomUserManager
 from django.core.validators import RegexValidator
 
+
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=30, unique=True, 
-                                validators=[RegexValidator(regex=r"^\d{10}$")])
+    username = models.CharField(
+        max_length=30, unique=True, validators=[RegexValidator(regex=r"^\d{10}$")]
+    )
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["phone_number",]
+    REQUIRED_FIELDS = [
+        "phone_number",
+    ]
 
     objects = CustomUserManager()
 
     def __str__(self):
         return self.phone_number + " " + self.username
-    
